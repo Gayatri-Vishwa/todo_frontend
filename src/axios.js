14a3d84821fb5f3e96253a945c43c5a3c237cd03
gayatriDev02 <blossomjasmine02@gmail.com>
2026-02-13 09:32:31 +0530

    Initial commit

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+// const instance = axios.create({
+//   baseURL:https:"todo-backend-sable-nu.vercel.app" ;   /// process.env.REACT_APP_BACKEND_URL, // backend URL from .env
+//   withCredentials: true, // only if your backend uses cookies
+// });
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL, // backend URL from .env
-  withCredentials: true, // only if your backend uses cookies
+  baseURL: "https://todo-backend-sable-nu.vercel.app",
+  withCredentials: true,
 });
 
 export default instance;
